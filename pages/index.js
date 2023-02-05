706419a5018d6a810d2089a4b5dcfbd899078702
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2023-02-05 14:23:36 +0530

    [UPDATE]modified styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
 
 const Landing = () => {
   const [showModal,setShowModal]=useState(false);
   const [show,setShow]=useState(false);
+  let router=useRouter();
   useEffect(()=>{
     setTimeout(()=>{
       setShow(true)
@@ -30,7 +32,7 @@ const Landing = () => {
          <div className='text-2xl font-medium'> Join Us</div>
          <div className='flex'>
 
-         <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">As a Donor</button>
+         <button onClick={()=>router.push('/donor/sdkj123AAz$kd')} type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">As a Donor</button>
          <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">As a FoodBank</button>
          </div>
       </div>
