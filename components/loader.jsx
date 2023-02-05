dc0bd6fb7b7f0e09df0ebb516ef225f40bb8ed1f
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2023-02-05 12:01:30 +0530

    [ADDED]landing screen ui

---
+++ components/loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <>
+    <div class="holder">
+  <div class="preloader"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+</div>
+    </>
+  )
+}
+
+export default Loading
